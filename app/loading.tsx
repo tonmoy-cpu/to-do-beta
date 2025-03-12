2e12c86286618b7eb5b7e05beb13c28519c4cf8a
Tonmoy mukherjee <tonmoymukherjee31@gmail.com>
2025-03-12 15:39:07 +0530

    another one

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,3 +1,4 @@
+"use client"
 export default function Loading() {
   return <div>Loading...</div>;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client"  
 import TaskManager from "@/components/pages/Task";
 
 export default function Home() {
