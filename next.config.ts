816a777c5d908e7055ffdd8ce8f601627d7b142c
Tonmoy mukherjee <tonmoymukherjee31@gmail.com>
2025-03-12 16:06:53 +0530

    another try hehe

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    cpus: 1,
+    workerThreads: false
+  }
 };
 
-export default nextConfig;
+module.exports = nextConfig;
