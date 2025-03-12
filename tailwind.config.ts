d11e10e12fe5ad2267f6ccafd4a6c381d7db2767
Tonmoy mukherjee <tonmoymukherjee31@gmail.com>
2025-03-12 17:10:29 +0530

    khandan randi to ban gya

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,7 @@ const config = {
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
-    "*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{ts,tsx}"
   ],
   prefix: "",
   theme: {
